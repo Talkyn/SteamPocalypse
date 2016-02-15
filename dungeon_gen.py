fcbe542a82f5f6e0ac8f60206a74df4ad1aed96c
Talkyn <hashiba.hideyoshi@gmail.com>
2016-02-15 07:44:25 -0800

    Opened DungeonGen branch

---
+++ dungeon_gen.py
@@ -0,0 +1,14 @@
+import settings
+
+
+
+"""
+if __name__ == "__main__":
+    import libtcodpy as tcod
+    
+    tcod.console_init_root(settings.SCREEN_W, settings.SCREEN_H, 'SP DungeonGen Tests')
+    tcod.sys_set_fps(20)
+    
+    while not tcod.console_is_window_closed():
+        # Render the dungeon
+"""
